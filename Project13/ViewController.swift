1b382479e69047c08f8fa03e808fbc18425e582a
gitkoDev <prostodenis1397@gmail.com>
2022-07-15 12:37:02 +0800

    add image view fade in animation

--- Project13/ViewController.swift
+++ Project13/ViewController.swift
@@ -106,6 +106,12 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         dismiss(animated: true)
         currentImage = image
         
+        imageView.alpha = 0
+        
+        UIView.animate(withDuration: 1, delay: 0, options: [], animations: {
+            self.imageView.alpha = 1
+        }, completion: nil)
+        
         let beginImage = CIImage(image: currentImage)
         currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
         
